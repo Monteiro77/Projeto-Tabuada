a82f90bf41aead0f03574187820e012db8a7937f
Vinícius Monteiro <vmonteiro7843@gmail.com>
2022-09-01 16:54:58 -0300

    Criação de variáveis

---
+++ src/br/senai/sp/jandira/model/Usuário.java
@@ -0,0 +1,21 @@
+package br.senai.sp.jandira.model;
+
+public class Usuário {
+	
+		
+		public int multiplicando;
+		public int minimoMultiplicador;
+		public int maximoMultiplicador;
+		public int multiplicador;
+		
+		
+		
+		public double getResultado() {
+			return multiplicando * minimoMultiplicador;
+			
+			
+			
+		}
+	
+
+}
