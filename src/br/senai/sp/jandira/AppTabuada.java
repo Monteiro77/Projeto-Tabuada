0a06382dddd5fc0104020a8cdab7922ab997393e
Vinícius Monteiro <vmonteiro7843@gmail.com>
2022-09-06 16:59:24 -0300

    adcionado as contas

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,10 +1,25 @@
 package br.senai.sp.jandira;
 
-import br.senai.sp.jandira.model.*;
+import br.senai.sp.jandira.model.Usuario;
 
 public class AppTabuada {
 
 	public static void main(String[] args) {
+		
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.multiplicando = 5;
+		usuario.minimoMultiplicador = 7;
+		usuario.maximoMultiplicador = 45;
+		
+		String[] resultado = usuario.getTabuada();
+		
+		int i = 0;
+		 while(i < resultado.length) {
+			 System.out.println(resultado[i]);
+			 i++;
+		 }
 		
 	 
 	}
---
+++ src/br/senai/sp/jandira/model/Usuario.java
@@ -0,0 +1,35 @@
+package br.senai.sp.jandira.model;
+
+	public class Usuario {
+		
+			
+			public int multiplicando;
+			public int minimoMultiplicador;
+			public int maximoMultiplicador;
+			
+			
+			
+			
+			public String[] getTabuada() {
+				
+				int tamanho = maximoMultiplicador - minimoMultiplicador + 1;
+				String[] tabuada = new String[tamanho];
+				int i = 0;
+				
+				while(minimoMultiplicador <= maximoMultiplicador) {
+					
+					int produto = multiplicando * minimoMultiplicador;
+					
+					String resultado = multiplicando + " X " + minimoMultiplicador + " = " + produto;
+					tabuada[i] = resultado;
+					
+					i++;
+					minimoMultiplicador++;
+				}
+				
+				return tabuada;
+			
+			}
+			
+			
+}
